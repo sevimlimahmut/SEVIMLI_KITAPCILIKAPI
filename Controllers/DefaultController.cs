c645d2cca89f271ffb4acda08fa791a2fbfa42fd
Mahmut SEVİMLİ <sevimlimahmut4@gmail.com>
2023-01-09 20:40:26 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,42 @@
+﻿using SEVIMLI_KITAPCILIKAPI.Models.DTO;
+using SEVIMLI_KITAPCILIKAPI.Models.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Web.Http;
+
+namespace SEVIMLI_KITAPCILIKAPI.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        Entities db = null;
+        public DefaultController()
+        {
+            db = new Entities();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/kitap/getall")]
+        public List<Class1> GetListOfSEVİMLİ_KİTAPCİLİK()
+        {
+            List<Class1> getkitaplist = db.SEVİMLİ_KİTAPCİLİK.Select(x => new Class1()
+            {
+
+                _id = x.İD,
+                _eser = x.ESERİN_ADİ,
+                _yazar = x.ESERİN_YAZARI,
+                _konum = x.KONUMU,
+                _adet = x.ADEDİ,
+                _fiyat= (int)x.FİYATİ
+
+
+
+
+            }).ToList();
+            return getkitaplist;
+        }
+    }
+}
---
+++ Models/DTO/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEVIMLI_KITAPCILIKAPI.Models.DTO
+{
+    public class Class1
+    {
+        private int Id;
+        public int _id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        private string Eser;
+        public string _eser
+        {
+            get { return Eser; }
+            set { Eser = value; }
+        }
+        private string Yazar;
+        public string _yazar
+        {
+            get { return Yazar; }
+            set { Yazar = value; }
+        }
+        private string Konum;
+        public string _konum
+        {
+            get { return Konum; }
+            set { Konum = value; }
+        }
+        private int Fiyat;
+        public int _fiyat
+        {
+            get { return Fiyat; }
+            set { Fiyat = value; }
+        }
+        private int Adet;
+        public int _adet
+        {
+            get { return Adet; }
+            set { Adet = value; }
+        }
+
+    }
+}
